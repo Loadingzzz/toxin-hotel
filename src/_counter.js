32d40b44017bd44880fc729605042908b9256fa7
1 <privetpokanet@gmail.com>
2023-02-24 00:13:25 +0300

    basicDropdown-over

--- src/_counter.js
+++ src/_counter.js
@@ -1,104 +1,92 @@
-
-const myCounter = document.querySelectorAll(['.counter__button']);
-
-myCounter.forEach (btn2 => {
-    btn2.addEventListener('click', function(){
-        const direction = this.dataset.direction;
-        const inp = this.parentElement.querySelector('.counter__value');
-        const currentValue = +inp.innerText;
-        let newValue;
-
-        if (direction === 'plus') {
-            newValue = currentValue +1;
-            let ButtonBCOne = document.querySelector('.counter__button-minus-one');
-            console.log(ButtonBCOne)
-            // let ButtonBCTwo = document.querySelector('.counter__button-minus-two');
-            // console.log(ButtonBCTwo)
-            // let ButtonBCThree = document.querySelector('.counter__button-minus-three');
-            // console.log(ButtonBCThree)
-            ButtonBCOne.classList.remove('counter__button-minus');
-        }
-        else {
-            newValue = currentValue -1 > 0 ? currentValue -1 : 0;
-            
-        }
-        inp.innerText = newValue;
-    });
-});
-
-window.addEventListener('click', function (event) {
+const globalCounter = document.querySelectorAll('.dropdown').forEach(function (counterWrapper) {
 
 
+
+
+    const myCounter = counterWrapper.querySelectorAll(['.counter__button']);
+
+    const counterOne = counterWrapper.querySelector('.counter__value-one');
+    const counterTwo = counterWrapper.querySelector('.counter__value-two');
+    const counterThree = counterWrapper.querySelector('.counter__value-three');
+    const guestQuantity = counterWrapper.querySelector('.guestQuantity');
+    const counterMenu = counterWrapper.querySelector('.menu__buttons');
     
+    
+      
+            
 
 
+    myCounter.forEach (btn => {
+        btn.addEventListener('click', function(){
+           const direction = this.dataset.direction;
+            const inp = this.parentElement.querySelector('.counter__value');
+           const currentValue = +inp.innerText;
+           let newValue;
 
+          if (direction === 'plus') {
+               newValue = currentValue +1;
+               
+           }
+           else {
+              newValue = currentValue -1 > 0 ? currentValue -1 : 0;
 
-    if (event.target.hasAttribute('data-cart')) {
-        console.log('Click')
+          }
+          inp.innerText = newValue;
+      });
+    });
 
-        const counterOne = document.querySelector('.counter__value-one');
-        console.log(counterOne);
-        const counterTwo = document.querySelector('.counter__value-two');
-        console.log(counterTwo);
-        const counterThree = document.querySelector('.counter__value-three');
-        console.log(counterThree);
-        const guestQuantity = document.querySelector('.guestQuantity');
-        console.log(guestQuantity.innerText);
-
-        
-
-        let innerTextOne = counterOne.innerText;
-        let innerTextTwo = counterTwo.innerText;
-        let innerTextThree = counterThree.innerText;
-        let innerNumOne =  Number.parseInt(innerTextOne);
-        let innerNumTwo =  Number.parseInt(innerTextTwo);
+    window.addEventListener('click', function (event) {
+         
+     if (event.target.hasAttribute('data-cart')) {
+         console.log('Click')
+         
+         let innerTextOne = counterOne.innerText;
+         let innerTextTwo = counterTwo.innerText;
+         let innerTextThree = counterThree.innerText;
+         let innerNumOne =  Number.parseInt(innerTextOne);
+         let innerNumTwo =  Number.parseInt(innerTextTwo);
         let innerNumThree =  Number.parseInt(innerTextThree);
-        console.log(typeof(innerTextOne,innerTextTwo,innerTextThree))
-
+        
         getSum = innerNumOne+innerNumTwo+innerNumThree;
-        console.log(getSum);
+         console.log(getSum);
 
+         if (getSum>=1) {
+             guestQuantity.innerText = getSum + " гостя";
+             counterMenu.classList.add('menu__buttons-rToClean');
+            counterMenu.querySelector('.menu__button-clean').classList.add('menu__button-clean_active');
+              
+         }
+         if (getSum>=5) {
+             guestQuantity.innerText = getSum + " гостей";
+         }
+         if (getSum<=1) {
+             guestQuantity.innerText = getSum +  " гость";
+         }
+         if (getSum<1) {
+             guestQuantity.innerText = "Сколько гостей";
+             counterMenu.classList.remove('menu__buttons-rToClean');
+            counterMenu.querySelector('.menu__button-clean').classList.remove('menu__button-clean_active');
+         }
 
+         
+     };
 
-        if (getSum>1) {
-            guestQuantity.innerText = getSum + " гостя";
-        }
-        if (getSum>=5) {
-            guestQuantity.innerText = getSum + " гостей";
-        }
-        if (getSum<=1) {
-            guestQuantity.innerText = getSum +  " гость";
-        }
-        if (getSum<1) {
-            guestQuantity.innerText = "Сколько гостей";
-        }
-    };
+      if(event.target.hasAttribute('data-clean')){
 
-    if(event.target.hasAttribute('data-clean')){
-        const guestQuantity = document.querySelector('.guestQuantity');
-        console.log(guestQuantity.innerText);
+          const guestQuantity = counterWrapper.querySelector('.guestQuantity');
 
+          counterOne.innerText = "0"
+          counterTwo.innerText = "0"
+          counterThree.innerText ="0"
 
-        const counterOne = document.querySelector('.counter__value-one');
-        console.log(counterOne);
-        const counterTwo = document.querySelector('.counter__value-two');
-        console.log(counterTwo);
-        const counterThree = document.querySelector('.counter__value-three');
-        console.log(counterThree);
+          counterMenu.classList.remove('menu__buttons-rToClean');
+          counterMenu.querySelector('.menu__button-clean').classList.remove('menu__button-clean_active');
+          guestQuantity.innerText = "Сколько гостей"
 
-        let innerTextOne = counterOne.innerText;
-        let innerTextTwo = counterTwo.innerText;
-        let innerTextThree = counterThree.innerText;
+      };
+      });
 
-        counterOne.innerText = "0"
-        counterTwo.innerText = "0"
-        counterThree.innerText ="0"
-
-        guestQuantity.innerText = "Сколько гостей"
-
-    };
-});
+    });
 
 
 
--- src/_dropdown.js
+++ src/_dropdown.js
@@ -1,33 +1,128 @@
-const myDropdown = document.querySelectorAll('.dropdown');
+const dropDown = document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
 
-myDropdown.forEach(dropdown => {
 
-    const select = document.querySelector('.select');
-    const caret = document.querySelector('.caret');
-    const menu = document.querySelector('.menu');
-    const options = document.querySelector('.menu li');
-    const selected = document.querySelector('.selected');
-    
-    select.addEventListener('click', () => {
-        select.classList.toggle('select-clicked');
-        caret.classList.toggle('caret-rotate');
-        menu.classList.toggle('menu-open');
-    });
 
-    options.forEach(option => {
-        option.addEventListener('click', () => {
 
-            selected.innerText = option.innerText;
-            select.classList.remove('select-cliked');
-            caret.classList.remove('caret-rotate');
-            menu.classList.remove('menu-open');
 
-            option.forEach(option => {
-                option.classList.remove('active');
-            });
-            option.classList.add('active');
+    const dropDownBtn = dropDownWrapper.querySelector('.selected');
+    const dropDownList = dropDownWrapper.querySelector('.menu');
+    const dropDownItem = dropDownList.querySelectorAll('.menu__item');
 
-        });
-    });
 
+
+    dropDownBtn.addEventListener('click', function (){
+        dropDownList.classList.toggle('menu-open');
+        this.classList.toggle('select-clicked')
+    })
+
+    dropDownItem.forEach(function(listItem){
+        listItem.addEventListener('click', function (e) {
+            e.stopPropagation();
+        })
+    })
+
+
+
+
+    // document.addEventListener('click', function (e) {
+    //     console.log('document click');
+    //    if (e.target == dropDownBtn) {
+    //        return
+    //    } else {
+    //     document.querySelector('.menu').classList.remove('menu-open');
+    //     document.querySelector('.selected').classList.remove('select-clicked');
+    //    }
+    // })
+    // windows.onclick = e => {
+    //     if (e.target == dropDownBtn) {
+    //                return
+    //            } else {
+    //             document.querySelector('.menu').classList.remove('menu-open');
+    //             document.querySelector('.selected').classList.remove('select-clicked');
+    //            
+
+    document.addEventListener('keydown', function (e) {
+       if (e.key === 'Tab' || e.key === 'Escape') {
+          dropDownList.classList.remove('menu-open');
+          dropDownBtn.classList.remove('select-clicked')
+
+       }
+    })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelectorAll('.dropdown').forEach(function (dropdownWrapper){
+
+
+
+// const myDropdown = document.querySelectorAll(['.select']);
+
+// myDropdown.forEach(dropdown => {
+    
+//     const select = document.querySelector('.select');
+//     const caret = document.querySelector('.caret');
+//     const menu = document.querySelector('.menu');
+//     const options = document.querySelector('.menu li');
+//     const selected = document.querySelector('.selected');
+    
+//     select.addEventListener('click', () => {
+//         console.log('fuck')
+//         select.classList.toggle('select-clicked');
+//         caret.classList.toggle('caret-rotate');
+//         menu.classList.toggle('menu-open');
+//     });
+
+//     options.forEach(option => {
+//         option.addEventListener('click', () => {
+
+//             selected.innerText = option.innerText;
+//             select.classList.remove('select-cliked');
+//             caret.classList.remove('caret-rotate');
+//             menu.classList.remove('menu-open');
+
+//             option.forEach(option => {
+//                 option.classList.remove('active');
+//             });
+//             option.classList.add('active');
+
+//         });
+//     });
+
+//     });
+// });
+
+
+
+
+
+
+
+document.querySelector('.selct').addEventListener('click', function (){
+    document.querySelector('.menu').classList.add('menu-open');
+})
+
+
+
+
