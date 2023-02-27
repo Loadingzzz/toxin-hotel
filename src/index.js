072a803c52301a44a96e3f45cdd32b18ac8d7305
1 <privetpokanet@gmail.com>
2023-02-27 17:10:53 +0300

    Checkbox+CheckboxRich+Toggle+Radio

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-
+import './form-elements.pug';
 import './color-and-types.pug';
 import './index.pug';
 import './pug/_head.pug';
 import './styles/index.scss';
 import './scripts/_input-email.js';
-import './form-elements.pug';
+
 import './scripts/_counter.js';
 import './scripts/_dropdown.js';
 import './scripts/_custom-dropdown.js';
